d7df378425424960a89f9910ce8998e82e26f6c5
maxblaul <144166736+maxblaul@users.noreply.github.com>
2023-11-20 20:55:05 -0600

    making some fixes

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -26,6 +26,10 @@ module.exports = () => {
     module: {
       rules: [
         
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
       ],
     },
   };
