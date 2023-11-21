b71581548a418bf8476a6b13d4d71d891882fe50
maxblaul <144166736+maxblaul@users.noreply.github.com>
2023-11-20 22:43:25 -0600

    fixing install, webpack, and src

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -20,6 +20,29 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Webpack Plugin',
+      }),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
+      new WebpackPwaManifest({
+        name: 'Text Editor PWA',
+        short_name: 'TextEditor',
+        description: 'Text Editor Progressive Web Application',
+        orientation: "portrait",
+        display: "standalone",
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512]
+          },
+        ]
+      })
       
     ],
 
@@ -29,6 +52,16 @@ module.exports = () => {
         {
           test: /\.css$/,
           use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
         },
       ],
     },
